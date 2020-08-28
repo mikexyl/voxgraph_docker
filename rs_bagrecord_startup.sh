47874cdf26e0a7d8123a79e9abd9e79d1c7dff25
Mike Xiangyu Liu <xiangyu002@e.ntu.edu.sg>
2020-08-28 15:46:44 +0800

    wait 10s after rs_rgbd for rs starting up

--- rs_bagrecord_startup.sh
+++ rs_bagrecord_startup.sh
@@ -4,4 +4,6 @@ source /ros_entrypoint.sh
 source ${HOME}/Workspace/maskgraph_ws/devel/setup.bash
 source ${HOME}/Workspace/orbslam_ws/devel/setup.bash
 roscore &
-roslaunch /rs_bagrecord.launch --wait
+roslaunch realsense2_camera rs_rgbd.launch --wait
+sleep 10
+roslaunch voxgraph orbslam_d435i_bagrecord.launch --wait
